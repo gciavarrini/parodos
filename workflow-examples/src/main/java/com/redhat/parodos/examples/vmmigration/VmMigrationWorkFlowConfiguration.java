55e8e0fbac3e02feb02b3d391e96217c3b738300
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-05-15 18:51:48 +0200

    missing annotation (to not forget)

--- workflow-examples/src/main/java/com/redhat/parodos/examples/vmmigration/VmMigrationWorkFlowConfiguration.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/vmmigration/VmMigrationWorkFlowConfiguration.java
@@ -14,8 +14,10 @@ import com.redhat.parodos.workflows.workflow.WorkFlow;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:git.properties")
 public class VmMigrationWorkFlowConfiguration {
 
 	@Bean
