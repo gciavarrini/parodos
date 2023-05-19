615abe5af7c0fd8b2e219dfad01fb0a78506f057
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-05-19 09:40:18 +0200

    ControllerExceptionHandler returns ErrorMessageDTO
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- notification-service/src/main/java/com/redhat/parodos/notification/common/controller/ControllerExceptionHandler.java
+++ notification-service/src/main/java/com/redhat/parodos/notification/common/controller/ControllerExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.redhat.parodos.notification.common.controller;
 
 import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import javax.validation.ConstraintViolationException;
 
 import com.redhat.parodos.notification.common.controller.exceptions.ApiValidationError;
+import lombok.Builder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,20 +23,24 @@ public class ControllerExceptionHandler {
 	@Autowired
 	private MessageSource messageSource;
 
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(ConstraintViolationException.class)
-	public @ResponseBody Map<String, Object> handleConstraintViolation(ConstraintViolationException e,
-			ServletWebRequest request) {
-		// emulate Spring DefaultErrorAttributes
-		final Map<String, Object> result = new LinkedHashMap<>();
-		result.put("timestamp", new Date());
-		result.put("path", request.getRequest().getRequestURI());
-		result.put("status", HttpStatus.BAD_REQUEST.value());
-		result.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
-		result.put("message", e.getMessage());
-		result.put("errors", e.getConstraintViolations().stream()
-				.map(cv -> ApiValidationError.from(cv, messageSource, request.getLocale())));
-		return result;
+	@ResponseBody
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorMessageDTO handleConstraintViolation(ConstraintViolationException e, ServletWebRequest request) {
+		// @formatter:off
+        return new ErrorMessageDTO.ErrorMessageDTOBuilder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(new Date())
+                .path(request.getRequest().getRequestURI())
+                .message(e.getMessage())
+                .description(StringUtils.collectionToCommaDelimitedString(e.getConstraintViolations().stream()
+                        .map(cv -> ApiValidationError.from(cv, messageSource, request.getLocale())).toList()))
+                .build();
+        // @formatter:on
+	}
+
+	@Builder
+	record ErrorMessageDTO(int status, Date timestamp, String path, String message, String description) {
 	}
 
 }
