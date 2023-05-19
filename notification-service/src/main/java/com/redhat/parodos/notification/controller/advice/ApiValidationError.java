517e2d19a6ac7d29e5a5516a94f285e10d836e5a
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-05-19 10:00:21 +0200

    Create ApiValidationError in notification-service
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>
    Signed-off-by: Nir Argaman <nargaman@redhat.com>

---
+++ notification-service/src/main/java/com/redhat/parodos/notification/controller/advice/ApiValidationError.java
@@ -0,0 +1,39 @@
+package com.redhat.parodos.notification.controller.advice;
+
+import java.beans.Introspector;
+import java.util.Locale;
+
+import javax.validation.ConstraintViolation;
+
+import lombok.*;
+
+import org.springframework.context.MessageSource;
+
+@Getter
+@ToString
+public class ApiValidationError {
+
+	String defaultMessage;
+
+	String objectName;
+
+	String field;
+
+	Object rejectedValue;
+
+	String code;
+
+	public static ApiValidationError from(ConstraintViolation<?> violation, MessageSource msgSrc, Locale locale) {
+		ApiValidationError result = new ApiValidationError();
+		result.defaultMessage = msgSrc.getMessage(violation.getMessageTemplate(),
+				new Object[] { violation.getLeafBean().getClass().getSimpleName(),
+						violation.getPropertyPath().toString(), violation.getInvalidValue() },
+				violation.getMessage(), locale);
+		result.objectName = Introspector.decapitalize(violation.getRootBean().getClass().getSimpleName());
+		result.field = String.valueOf(violation.getPropertyPath());
+		result.rejectedValue = violation.getInvalidValue();
+		result.code = violation.getMessageTemplate();
+		return result;
+	}
+
+}
